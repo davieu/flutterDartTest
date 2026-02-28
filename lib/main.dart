d402f303e0b2b5d6339b1c4dafe729773297497b
davieu <davisumana@gmail.com>
2026-02-27 22:44:15 -0500

    Removed testfile, added gradient_container class file

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-    MaterialApp(
+    const MaterialApp(
       home: Scaffold(
         body: GradientContainer(),
       ),
@@ -11,6 +11,14 @@ void main() {
 }
 
 class GradientContainer extends StatelessWidget {
+  const GradientContainer({super.key});
+  //   GradientContainer({key}) : super(key: key)
+  //             {named arg}   (key from Stateless widg: key from names arg)
+  // put curly braces if you want to use named arguments
+  // super refers to the parent class. In this case StatelessWidget
+  // or
+  //GradientContainer() {}
+
   @override
   Widget build(context) {
     return Container(
